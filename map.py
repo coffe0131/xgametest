86bc964041df40aec68ff47803be2063193e72d3
coffee0131 <ragnine9@gmail.com>
2020-05-06 10:25:52 +0900

    맵

--- map.py
+++ map.py
@@ -9,7 +9,6 @@ class BACKGROUND:
         self.white = (255,255,255)
         self.background_width=944
         self.background1=pygame.image.load('images/background4.png')
-        print(self.background1.get_rect())
         self.background1 = pygame.transform.scale(self.background1,(1000,500))
         self.background2=self.background1.copy()
         self.background1_x = 0
