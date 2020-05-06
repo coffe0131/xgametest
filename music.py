9e40653d6adfbbcab65bc16dfd000bf56d5acac3
coffee0131 <ragnine9@gmail.com>
2020-05-06 10:26:21 +0900

    음악재생

--- music.py
+++ music.py
@@ -1,11 +1,21 @@
 import pygame
 from pygame import *
 
-def mplay(sound):
-    pygame.mixer.init()
-    pygame.mixer.music.load('music/Ice_Cream_Island.mp3')
-    pygame.mixer.music.set_volume(sound)
-    pygame.mixer.music.play(-1)
 
+pygame.mixer.init()
+musics=['music/Ice_Cream_Island.mp3', 'music/boss.mp3']
+
+class MUSIC:
+    def __init__(self):
+        self.music=pygame.mixer.music.load(musics[0])
+        self.jump=pygame.mixer.Sound('music/jump.wav')
+        self.item=pygame.mixer.Sound('music/itemget.wav')
+        self.boom=pygame.mixer.Sound('music/fireattack.wav')
+        self.fire=pygame.mixer.Sound('music/boom.wav')
+
+    def mplay(self,i):
+        self.music = pygame.mixer.music.load(musics[i])
+        pygame.mixer.music.set_volume(0.35)
+        pygame.mixer.music.play(-1)
 
 
